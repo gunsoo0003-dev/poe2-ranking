10d62fefd340131e18ee36cdf6be82bc22a6fd75
gunsoo0003-dev <gunsoo0003@gmail.com>
2026-05-29 21:56:54 +0900

    add data home to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,16 +5,22 @@ const siteUrl = 'https://data.fixlgs.com';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
+      url: siteUrl,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
       url: `${siteUrl}/poe2`,
       lastModified: new Date(),
       changeFrequency: 'daily',
-      priority: 1,
+      priority: 0.9,
     },
     {
       url: `${siteUrl}/poe2/en`,
       lastModified: new Date(),
       changeFrequency: 'daily',
-      priority: 0.9,
+      priority: 0.8,
     },
   ];
 }
